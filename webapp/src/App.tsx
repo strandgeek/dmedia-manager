bd1fc06be88e1eb13737e7fec3175b8af556c2a4
strandgeek <strandgeek@gmail.com>
2022-05-06 00:55:19 -0300

    feat(webapp): add tailwind and initial (and temporary) home page

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import homeLogo from "./assets/home-logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="mx-auto max-w-lg text-center">
+        <div className="mt-24">
+          <img className="h-36 inline-block" src={homeLogo} alt="dMedia Manager" />
+          <span className="sr-only">dMedia Manager</span>
+        </div>
+        <div className="mt-16">
+          <button
+            type="button"
+            className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Connect Your Wallet
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
