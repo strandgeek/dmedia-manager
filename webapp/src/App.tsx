497b4419e8c082dc85862ccd8178505c328ca5c8
strandgeek <strandgeek@gmail.com>
2022-05-06 01:24:16 -0300

    feat(webapp): improve home page

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,24 +1,9 @@
 import React from "react";
-import homeLogo from "./assets/home-logo.png";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <div className="mx-auto max-w-lg text-center">
-        <div className="mt-24">
-          <img className="h-36 inline-block" src={homeLogo} alt="dMedia Manager" />
-          <span className="sr-only">dMedia Manager</span>
-        </div>
-        <div className="mt-16">
-          <button
-            type="button"
-            className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Connect Your Wallet
-          </button>
-        </div>
-      </div>
-    </div>
+    <Home />
   );
 }
 
