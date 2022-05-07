9cf7d455dc11c082ba22d32257c85048d597c214
strandgeek <strandgeek@gmail.com>
2022-05-07 00:11:36 -0300

    style(api): improve code style

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,18 +1,16 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import cors from "cors";
 
 // Constants
-const PORT = 3000;
+const PORT = 4000;
 
 // Express App Setup
 const app: Application = express();
 
 // Global Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 // API V1
