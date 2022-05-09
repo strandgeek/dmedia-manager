f5a7e464f2a705b33d21069d6dc6736f15e7b33e
strandgeek <strandgeek@gmail.com>
2022-05-08 21:57:06 -0300

    chore(web): organize all web packages

---
+++ web/packages/react-dmedia-manager/src/index.ts
@@ -0,0 +1,2 @@
+export { default as Hello } from './components/hello'; // export the default export from './hello', this is Hello component
+export * from './components/hello'; // export all named exports from './hello' like HelloProps
