8de4bb75e369c7d89555095e55faacd9c118c0f6
strandgeek <strandgeek@gmail.com>
2022-05-18 01:54:55 -0300

    fix: add missing trigger for access control setup button

--- web/packages/webapp/src/pages/admin/Settings.tsx
+++ web/packages/webapp/src/pages/admin/Settings.tsx
@@ -84,6 +84,7 @@ export const AdminSettings = () => {
                 </div>
                 <button
                   type="button"
+                  onClick={() => setSetupAccessControlOpen(true)}
                   className="w-full text-center justify-center inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   <CogIcon className="w-6 h-6 mr-2" />
